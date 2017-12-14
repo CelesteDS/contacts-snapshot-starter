b7c8316dca0e7b18f209d6d2b6f59b6aa0a103d0
Celeste De Santiago <celestedaphne@gmail.com>
2017-12-13 21:07:50 -0800

    Add initial Nightmare tests

---
+++ test/ui/contacts.test.js
@@ -0,0 +1,63 @@
+const Nightmare = require('nightmare')
+const { expect } = require('chai')
+
+describe('Load a Page', function () {
+  // Recommended: 5s locally, 10s to remote server, 30s from airplane ¯\_(ツ)_/¯
+  this.timeout('15s')
+
+  let nightmare = null
+  beforeEach(() => {
+    nightmare = new Nightmare()
+  })
+
+  describe('/ (Home Page)', () => {
+    it('should load without error', (done) => {
+      // your actual testing urls will likely be `http://localhost:port/path`
+      nightmare.goto('http://localhost:3000')
+        .end()
+        .then(result => done())
+        .catch(done)
+    })
+  })
+
+  describe('/contacts/new', () => {
+    it('should load without error', (done) => {
+      nightmare.goto('http://localhost:3000/contacts/new')
+        .end()
+        .then(() => done())
+    // have a question: above line throws an error if written .then(done) Shouldn't this do the same thing??
+        .catch(done)
+    })
+  })
+
+  describe('New Contact link', () => {
+    it('should redirect to new contact page', (done) => {
+      nightmare
+        .goto('http://localhost:3000')
+        .click('[href="/contacts/new"]')
+        .wait(3000)
+        .url()
+        .then((result) => {
+          expect(result).to.equal('http://localhost:3000/contacts/new')
+        })
+        .then(() => done())
+        .catch(done)
+    })
+  })
+  //
+  // describe('search', () => {
+  //   it('should redirect to contacts search route', (done) => {
+  //     nightmare
+  //       .goto('http://localhost:3000')
+  //       .type('form', 'Godot\u000d')
+  //       .wait(5000)
+  //       .url()
+  //       .end()
+  //       .then((result) => {
+  //         expect(result).to.equal('http://localhost:3000/contacts/search?q=Godot')
+  //       })
+  //       .then(() => done())
+  //       .catch(done)
+  //   })
+  // })
+})
